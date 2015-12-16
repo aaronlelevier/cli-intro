c49fab43928495459729c01963b317aa80fd10a5
aronysidoro <pyaaron@gmail.com>
2015-12-16 09:03:56 -0800

    first commit

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('users', {path: '/users'}, function() {
+    this.route('detail');
+  });
 });
 
 export default Router;
